711d7fbbfc52bfa9a6451e08eed41d739d41dc75
HermannKroll <hermann.kroll@gmail.com>
2023-11-10 11:41:11 +0100

    refactor recommender base

--- src/narrec/recommender/base.py
+++ src/narrec/recommender/base.py
@@ -1,19 +1,20 @@
+from abc import ABCMeta, abstractmethod
+
 from narrec.citation.graph import CitationGraph
 from narrec.document.document import RecommenderDocument
 
 
-class RecommenderBase:
+class RecommenderBase(ABCMeta):
 
-    def __init__(self, citation_graph: CitationGraph):
-        self.citation_graph: CitationGraph = citation_graph
-        pass
-
-    def recommend_documents(self, doc: RecommenderDocument, docs_from: [RecommenderDocument]) -> [RecommenderDocument]:
+    @abstractmethod
+    def recommend_documents(self, doc: RecommenderDocument, docs_from: [RecommenderDocument],
+                            citation_graph: CitationGraph) -> [RecommenderDocument]:
         """
         This abstract method must be implemented in a subclass. The goal is to create a ranked list of relevant
         documents from docs_from which should be recommended for doc
         :param doc: the document for which recommendations should be generated
         :param docs_from: the list of possible documents to recommend
+        :param citation_graph: citation network
         :return: a ranked list of recommended documents
         """
         raise NotImplementedError
