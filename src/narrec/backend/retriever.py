49df8fda2526cfc87ceb97b6a504e66737b1e145
HermannKroll <hermann.kroll@gmail.com>
2023-11-15 15:04:26 +0100

    add a document translation component (if we work with something else than PubMed

--- src/narrec/backend/retriever.py
+++ src/narrec/backend/retriever.py
@@ -1,6 +1,7 @@
 from typing import List
 
-from kgextractiontoolbox.backend.models import Document
+from kgextractiontoolbox.backend.database import Session
+from kgextractiontoolbox.backend.models import Document, DocumentTranslation
 from kgextractiontoolbox.backend.retrieve import iterate_over_all_documents_in_collection, \
     retrieve_narrative_documents_from_database
 from kgextractiontoolbox.document.document import TaggedEntity
@@ -9,6 +10,47 @@ from narraint.backend.database import SessionExtended
 from narrant.entity.entityresolver import GeneResolver
 from narrant.preprocessing.enttypes import GENE
 from narrec.document.document import RecommenderDocument
+
+
+class DocumentTranslator:
+
+    def __init__(self):
+        self.__doc_translation_source2art = {}
+        self.__doc_translation_art2source = {}
+
+    def __cache_document_translation(self, document_collection: str):
+        if document_collection not in self.__doc_translation_source2art:
+            session = Session.get()
+            query = session.query(DocumentTranslation.document_id, DocumentTranslation.source_doc_id)
+            query = query.filter(DocumentTranslation.document_collection == document_collection)
+
+            source2art = {}
+            art2source = {}
+            for r in query:
+                art2source[r.document_id] = r.source_doc_id
+                source2art[r.source_doc_id] = r.document_id
+
+            self.__doc_translation_source2art[document_collection] = source2art
+            self.__doc_translation_art2source[document_collection] = art2source
+
+    def translate_document_ids_art2source(self, document_ids: [int], document_collection: str):
+        # Hack for PubMed
+        if document_collection == "PubMed":
+            return {str(d) for d in document_ids}
+        self.__cache_document_translation(document_collection)
+        art2source = self.__doc_translation_art2source[document_collection]
+        return [art2source[int(d)] for d in document_ids if int(d) in art2source]
+
+    def translate_document_id_art2source(self, document_id: str, document_collection: str):
+        # Hack for PubMed
+        if document_collection == "PubMed":
+            return int(document_id)
+        return self.translate_document_ids_art2source([document_id], document_collection)[0]
+
+    def translate_document_ids_source2art(self, document_ids: [str], document_collection: str):
+        self.__cache_document_translation(document_collection)
+        source2art = self.__doc_translation_source2art[document_collection]
+        return [source2art[d] for d in document_ids if d in source2art]
 
 
 class DocumentRetriever:
