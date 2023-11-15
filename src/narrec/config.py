b7922a63f1fabb306d9bbb0c48b438053f451d85
HermannKroll <hermann.kroll@gmail.com>
2023-11-15 13:15:48 +0100

    move benchmark data

--- src/narrec/config.py
+++ src/narrec/config.py
@@ -4,8 +4,10 @@ GIT_ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__fi
 RESOURCE_DIR = os.path.join(GIT_ROOT_DIR, "resources")
 CONFIG_DIR = os.path.join(GIT_ROOT_DIR, "config")
 
-RELISH_BENCHMARK_FILE = os.path.join(RESOURCE_DIR, "RELISH_v1.json.gz")
+BENCHMARK_DIR = os.path.join(RESOURCE_DIR, "benchmarks")
 
+RELISH_BENCHMARK_FILE = os.path.join(BENCHMARK_DIR, "RELISH_v1.json.gz")
+PM2020_TOPIC_FILE = os.path.join(BENCHMARK_DIR, "trec_pm2020_topics.xml")
 
 # DB Backend
 BACKEND_CONFIG = os.path.join(CONFIG_DIR, "backend.json")
