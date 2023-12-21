107e49aa3b231c83444ba79466dfbddcf037748f
Bill Matthias Thang <m.thang@tu-bs.de>
2023-12-21 13:58:16 +0100

    added function to create relish benchmark file with rankings and updated config.py

--- src/narrec/benchmark/relish.py
+++ src/narrec/benchmark/relish.py
@@ -2,7 +2,7 @@ import json
 import logging
 
 from narrec.benchmark.benchmark import Benchmark, BenchmarkMode
-from narrec.config import RELISH_BENCHMARK_FILE
+from narrec.config import RELISH_BENCHMARK_FILE, RELISH_BENCHMARK_JSON_FILE
 from narrec.recommender.base import RecommenderBase
 
 
@@ -45,10 +45,10 @@ class RelishBenchmark(Benchmark):
         #     "experience": "phd-post-gt10",
         #     "is_anonymous": true
         # },
-        logging.info(f"Loading Relish benchmark data from {RELISH_BENCHMARK_FILE}")
+        logging.info(f"Loading Relish benchmark data from {RELISH_BENCHMARK_JSON_FILE}")
         self.doc2recommended.clear()
         self.doc2not_recommended.clear()
-        with open(RELISH_BENCHMARK_FILE, 'rt') as f:
+        with open(RELISH_BENCHMARK_JSON_FILE, 'rt') as f:
             relish_data = json.load(f)
 
         for idx, rating in enumerate(relish_data):
@@ -92,6 +92,33 @@ class RelishBenchmark(Benchmark):
         for idx, docid in self.documents_with_idx:
             relevant, irrelevant = self.get_evaluation_data_for_topic(idx, docid, mode)
 
+    @staticmethod
+    def process_json_to_txt():
+        with open(RELISH_BENCHMARK_JSON_FILE, 'r') as json_file:
+            data = json.load(json_file)
+
+        output_lines = []
+
+        for entry in data:
+            pmid = entry["pmid"]
+            relevant = entry["response"]["relevant"]
+            partial = entry["response"]["partial"]
+            irrelevant = entry["response"]["irrelevant"]
+
+            # Process relevant entries
+            output_lines.extend(f'{pmid} 0 {rel_id} 2' for rel_id in relevant)
+
+            # Process partial entries
+            output_lines.extend(f'{pmid} 0 {part_id} 1' for part_id in partial)
+
+            # Process irrelevant entries
+            output_lines.extend(f'{pmid} 0 {irrel_id} 0' for irrel_id in irrelevant)
+
+        with open(RELISH_BENCHMARK_FILE, 'w') as txt_file:
+            txt_file.write('\n'.join(output_lines))
+
+        logging.info(f'Document file created successfully.')
+
 
 # for test purposes
 if __name__ == '__main__':
@@ -99,4 +126,5 @@ if __name__ == '__main__':
                         datefmt='%Y-%m-%d:%H:%M:%S',
                         level=logging.DEBUG)
     benchmark = RelishBenchmark()
-    benchmark.load_benchmark_data()
+    #benchmark.load_benchmark_data()
+    benchmark.process_json_to_txt()
--- src/narrec/config.py
+++ src/narrec/config.py
@@ -6,9 +6,14 @@ CONFIG_DIR = os.path.join(GIT_ROOT_DIR, "config")
 
 BENCHMARK_DIR = os.path.join(RESOURCE_DIR, "benchmarks")
 
-RELISH_BENCHMARK_FILE = os.path.join(BENCHMARK_DIR, "RELISH_v1.json")
+RELISH_BENCHMARK_JSON_FILE = os.path.join(BENCHMARK_DIR, "RELISH_v1.json")
+RELISH_BENCHMARK_FILE = os.path.join(BENCHMARK_DIR, "RELISH_documents.txt")
 PM2020_BENCHMARK_FILE = os.path.join(BENCHMARK_DIR, "trec_pm2020_documents.txt")
 PM2020_TOPIC_FILE = os.path.join(BENCHMARK_DIR, "trec_pm2020_topics.xml")
 
+PMIDS_DIR = os.path.join(RESOURCE_DIR, "pmids")
+
+RELISH_PMIDS_FILE = os.path.join(PMIDS_DIR, "pmids_pm2018.txt")
+TG2005_PMIDS_FILE = os.path.join(PMIDS_DIR, "pmids_tg2005.txt")
 # DB Backend
 BACKEND_CONFIG = os.path.join(CONFIG_DIR, "backend.json")
