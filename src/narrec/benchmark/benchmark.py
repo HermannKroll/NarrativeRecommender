8334e99c95f31a9dd56e9f896a406a523c156bc3
HermannKroll <hermann.kroll@gmail.com>
2023-12-22 08:00:49 +0100

    implement loading document ids for benchmarks

--- src/narrec/benchmark/benchmark.py
+++ src/narrec/benchmark/benchmark.py
@@ -12,8 +12,11 @@ class BenchmarkMode(Enum):
 
 class Benchmark:
 
-    def __init__(self):
-        pass
+    def __init__(self, name, path_to_document_ids):
+        self.name = name
+        self.document_ids = set()
+        self.path_to_document_ids = path_to_document_ids
+        self.documents_for_baseline_load = False
 
     @abstractmethod
     def load_benchmark_data(self):
@@ -21,3 +24,15 @@ class Benchmark:
 
     def perform_evaluation(self, recommender: RecommenderBase, mode: BenchmarkMode):
         raise NotImplementedError
+
+
+    def get_documents_for_baseline(self):
+        if self.documents_for_baseline_load:
+            return self.document_ids
+        else:
+            self.document_ids = set()
+            with open(self.path_to_document_ids, 'rt') as f:
+                for line in f:
+                    self.document_ids.add(int(line.strip()))
+            print(f'Load {len(self.document_ids)} for {self.name}')
+        self.documents_for_baseline_load = True
