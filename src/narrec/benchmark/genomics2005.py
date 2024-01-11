9483444dc1c9e084b5b8c02428d9e1b0c880078b
HermannKroll <hermann.kroll@gmail.com>
2024-01-11 09:33:02 +0100

    count documents considered

--- src/narrec/benchmark/genomics2005.py
+++ src/narrec/benchmark/genomics2005.py
@@ -1,3 +1,5 @@
+import logging
+
 from narrec.benchmark.benchmark import Benchmark
 from narrec.config import TG2005_PMIDS_FILE
 
@@ -9,3 +11,16 @@ class Genomics2005(Benchmark):
 
     def load_benchmark_data(self):
         pass
+
+
+
+def main():
+    logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+                        datefmt='%Y-%m-%d:%H:%M:%S',
+                        level=logging.DEBUG)
+    benchmark = Genomics2005()
+    logging.info(f'Benchmark has {len(benchmark.get_documents_for_baseline())} documents')
+
+
+if __name__ == '__main__':
+    main()
