b06f24d2550764ae7ea496566e18e638e5645984
Bill Matthias Thang <m.thang@tu-bs.de>
2024-01-16 16:40:26 +0100

    Script for TREC evaluation using pytrec_eval

--- src/narrec/benchmark/eval.py
+++ src/narrec/benchmark/eval.py
@@ -1,4 +1,8 @@
-METRICS = [
+import pytrec_eval
+import json
+
+
+METRICS_OLD = [
     "recall@1000",
     "ndcg@10",
     "ndcg@20",
@@ -10,6 +14,18 @@ METRICS = [
     "precision@100"
 ]
 
+METRICS = {
+    'recall_1000',
+    'ndcg_cut_10',
+    'ndcg_cut_20',
+    'ndcg_cut_100',
+    'bpref',
+    'map_cut_1000',
+    'P_10',
+    'P_20',
+    'P_100'
+}
+
 
 def perform_evaluation_report(qrel_file: str, run_file: str, output: str):
     """
@@ -20,4 +36,15 @@ def perform_evaluation_report(qrel_file: str, run_file: str, output: str):
     :param output: write report results as a JSON to this path
     :return: None
     """
-    pass
+    #print(pytrec_eval.supported_measures)
+    with open(qrel_file, 'r') as f_qrel:
+        qrel = pytrec_eval.parse_qrel(f_qrel)
+
+    with open(run_file, 'r') as f_run:
+        run = pytrec_eval.parse_run(f_run)
+
+    evaluator = pytrec_eval.RelevanceEvaluator(qrel, METRICS)
+    results = evaluator.evaluate(run)
+
+    with open(output, 'w') as f_out:
+        json.dump(results, f_out, indent=4)
