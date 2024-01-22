622541f30f1d3636f4269d6c420a590f2ff506b7
HermannKroll <hermann.kroll@gmail.com>
2024-01-22 13:04:39 +0100

    retrieve 1001 docs (input will be retrieved)

--- src/narrec/config.py
+++ src/narrec/config.py
@@ -31,5 +31,5 @@ PM2020_PMIDS_FILE = os.path.join(DATA_DIR, "pmids_pm2020.txt")
 # DB Backend
 BACKEND_CONFIG = os.path.join(CONFIG_DIR, "backend.json")
 
-# Experimental Configuration
-BM25_DOCUMENT_CUTOFF = 1000
+# Experimental Configuration (because first stage will always find input doc)
+BM25_DOCUMENT_CUTOFF = 1001
