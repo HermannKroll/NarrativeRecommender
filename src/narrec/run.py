1e53f20ce7574b31f0fbc7304da7d678b3381d36
HermannKroll <hermann.kroll@gmail.com>
2024-01-22 13:04:55 +0100

    implement first stage retrieval + refactor code

--- src/narrec/run.py
+++ src/narrec/run.py
@@ -1,11 +1,16 @@
 import os
 from datetime import datetime
 
+from tqdm import tqdm
+
 from narrec.backend.retriever import DocumentRetriever
-from narrec.benchmark.benchmark import BenchmarkType
+from narrec.benchmark.benchmark import BenchmarkType, Benchmark
 from narrec.benchmark.relish import RelishBenchmark
 from narrec.citation.graph import CitationGraph
-from narrec.config import RESULT_DIR
+from narrec.config import RESULT_DIR, INDEX_DIR, GLOBAL_DB_DOCUMENT_COLLECTION
+from narrec.firststage.base import FirstStageBase
+from narrec.firststage.bm25abstract import BM25Abstract
+from narrec.firststage.bm25title import BM25Title
 from narrec.recommender.simple import RecommenderSimple
 
 
@@ -15,7 +20,7 @@ def load_document_ids_from_runfile(path_to_runfile):
         for line in f:
             components = line.split('\t')
             topic_id = int(components[0])
-            doc_id = components[2]
+            doc_id = int(components[2])
             score = float(components[4])
 
             if topic_id not in topic2docs:
@@ -26,58 +31,116 @@ def load_document_ids_from_runfile(path_to_runfile):
     return topic2docs
 
 
+def run_first_stage_for_benchmark(retriever: DocumentRetriever, benchmark: Benchmark, first_stage: FirstStageBase,
+                                  result_path: str):
+    print(f'Creating first stage runfile for benchmark: {benchmark.name} with stage: {first_stage.name}')
+    result_lines = []
+
+    print('Retrieve document contents...')
+    input_docs = retriever.retrieve_narrative_documents(document_ids=list(benchmark.get_input_document_ids()),
+                                                        document_collection=benchmark.document_collection)
+
+    print('Perform first stage retrieval')
+    for input_doc in tqdm(input_docs, total=len(input_docs)):
+        retrieved_docs = first_stage.retrieve_documents_for(input_doc)
+
+        for rank, (docid, score) in enumerate(retrieved_docs):
+            if int(docid) == input_doc.id:
+                continue
+
+            result_line = f'{input_doc.id}\tQ0\t{docid}\t{rank + 1}\t{score}\t{first_stage.name}'
+            result_lines.append(result_line)
+
+    print(f'Results will be written to {result_path}')
+    with open(result_path, 'wt') as f:
+        f.write('\n'.join([l for l in result_lines]))
+
+
 def main():
     benchmarks = [RelishBenchmark()]
     retriever = DocumentRetriever()
     citation_graph = CitationGraph()
-    first_stages = ["BM25"]
     recommenders = [RecommenderSimple()]
+    DO_RECOMMENDATION = False
 
     for bench in benchmarks:
+        index_path = os.path.join(INDEX_DIR, bench.name)
+        first_stages = [BM25Title(index_path), BM25Abstract(index_path)]
+
         for first_stage in first_stages:
-            fs_path = os.path.join(RESULT_DIR, first_stage)
-            fs_docs = load_document_ids_from_runfile(fs_path)
-
             if bench.type == BenchmarkType.REC_BENCHMARK:
-                for topic in bench.topics:
-                    documents = retriever.retrieve_narrative_documents(fs_docs, "PubMed")
-                    docid2doc = {d.id: d for d in documents}
+                fs_path = os.path.join(RESULT_DIR, f'{first_stage.name}.txt')
+                if not os.path.isfile(fs_path):
+                    # create first stage data
+                    run_first_stage_for_benchmark(retriever, bench, first_stage, fs_path)
+                # next load the documents for this first stage
+                print(f'Loading first stage runfile: {fs_path}')
+                fs_docs = load_document_ids_from_runfile(fs_path)
 
-                    for recommender in recommenders:
-                        start = datetime.now()
-                        rec_docs = recommender.recommend_documents(docid2doc[topic], documents, citation_graph)
+                for input_docid, retrieved_docs in fs_docs.items():
+                    # Retrieve the input document
+                    input_doc = retriever.retrieve_narrative_documents([input_docid], GLOBAL_DB_DOCUMENT_COLLECTION)[0]
 
-                        result_lines = []
-                        if len(rec_docs) > 0:
-                            max_score = rec_docs[0][1]
-                            if max_score < 0.0:
-                                raise ValueError(
-                                    f'Max score {max_score} <= (score = {max_score} / ranker = {recommender.name})')
+                    # Retrieve the documents to score
+                    retrieved_doc_ids = [d[0] for d in retrieved_docs]
+                    documents = retriever.retrieve_narrative_documents(retrieved_doc_ids, GLOBAL_DB_DOCUMENT_COLLECTION)
 
-                            for rank, (doc_id, score) in enumerate(rec_docs):
-                                if max_score > 0.0:
-                                    norm_score = score / max_score
-                                else:
-                                    norm_score = 0.0
+                    if DO_RECOMMENDATION:
+                        for recommender in recommenders:
+                            start = datetime.now()
+                            rec_docs = recommender.recommend_documents(input_doc, documents, citation_graph)
 
-                                if norm_score > 1.0 or norm_score < 0.0:
+                            result_lines = []
+                            if len(rec_docs) > 0:
+                                max_score = rec_docs[0][1]
+                                if max_score < 0.0:
                                     raise ValueError(
-                                        f'Document {doc_id} received a score not in [0, 1] (score = {norm_score} / ranker = {recommender.name})')
+                                        f'Max score {max_score} <= (score = {max_score} / ranker = {recommender.name})')
 
-                                result_line = f'{q.query_id}\tQ0\t{doc_id}\t{rank + 1}\t{norm_score}\t{recommender.name}'
-                                result_lines.append(result_line)
+                                for rank, (doc_id, score) in enumerate(rec_docs):
+                                    if max_score > 0.0:
+                                        norm_score = score / max_score
+                                    else:
+                                        norm_score = 0.0
 
-                        time_taken = datetime.now() - start
-                        print(f'{time_taken}s to compute {recommender.name}')
+                                    if norm_score > 1.0 or norm_score < 0.0:
+                                        raise ValueError(
+                                            f'Document {doc_id} received a score not in [0, 1] (score = {norm_score} / ranker = {recommender.name})')
 
-                        path = os.path.join(RESULT_DIR,
-                                            f'{bench.name}_{topic}_{recommender.name}.txt')
-                        with open(path, 'wt') as f:
-                            f.write('\n'.join(result_lines))
+                                    result_line = f'{input_docid}\tQ0\t{doc_id}\t{rank + 1}\t{norm_score}\t{recommender.name}'
+                                    result_lines.append(result_line)
+
+                            time_taken = datetime.now() - start
+                            print(f'{time_taken}s to compute {recommender.name}')
+
+                            path = os.path.join(RESULT_DIR,
+                                                f'{bench.name}_{input_docid}_{recommender.name}.txt')
+                            with open(path, 'wt') as f:
+                                f.write('\n'.join(result_lines))
 
 
             elif bench.type == BenchmarkType.IR_BENCHMARK:
                 raise NotImplementedError
+
+                # Idea: for each topic, get all relevant documents
+                # Select one of these documents
+                # perform the recommendation step
+                # results = []
+                # for relevant_document in relevant:
+                #     # Do recommendation
+                #     scores = []
+                #
+                #     # Needs to implement a first stage
+                #     # recommended_documents = recommender.recommend_documents(relevant_document, )
+                #
+                #     results.append((relevant_document, scores))
+                #
+                #     pass
+
+                # Average over scores
+
+                # Todo: implement some statistics here
+
             else:
                 raise NotImplementedError
 
