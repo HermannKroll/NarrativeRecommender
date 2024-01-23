3a4f22567c229712351e43be47f2576f4c0a749d
HermannKroll <hermann.kroll@gmail.com>
2024-01-23 12:05:15 +0100

    change weight + print config

--- src/narrec/run_config.py
+++ src/narrec/run_config.py
@@ -18,8 +18,9 @@ BENCHMARKS = [
 ]
 
 
-CONFIDENCE_WEIGHT = 0.5
-TFIDF_WEIGHT = 0.5
+CONFIDENCE_WEIGHT = 0.0
+TFIDF_WEIGHT = 1.0
+
 
 assert CONFIDENCE_WEIGHT + TFIDF_WEIGHT == 1.0
 
@@ -29,3 +30,10 @@ NARRATIVE_CORE_THRESHOLD = 0.5
 
 # Experimental Configuration (because first stage will always find input doc)
 FS_DOCUMENT_CUTOFF = 1001
+
+
+print('--'*60)
+print(f'Confidence weight : {CONFIDENCE_WEIGHT}')
+print(f'TF-IDF weight     : {TFIDF_WEIGHT}')
+print(f'Document FS cutoff: {FS_DOCUMENT_CUTOFF}')
+print('--'*60)
