dfc0365cbb249ef08dbe32a1759a22250201542f
HermannKroll <hermann.kroll@gmail.com>
2024-02-19 10:24:15 +0100

    remove filter + add basic score computation

--- src/narrec/scoring/flask/edge_debug.py
+++ src/narrec/scoring/flask/edge_debug.py
@@ -1,5 +1,4 @@
 import logging
-from collections import defaultdict
 
 from flask import Flask, render_template
 
@@ -7,11 +6,22 @@ from kgextractiontoolbox.backend.models import Predication
 from kgextractiontoolbox.backend.retrieve import retrieve_narrative_documents_from_database
 from narraint.backend.database import SessionExtended
 from narrant.entity.entityresolver import EntityResolver
+from narrec.backend.retriever import DocumentRetriever
+from narrec.document.corpus import DocumentCorpus
+from narrec.scoring.edge import score_edge, score_edge_sentence
 
 logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
                     datefmt='%Y-%m-%d:%H:%M:%S',
                     level=logging.INFO)
+
 resolver = EntityResolver.instance()
+retriever = DocumentRetriever()
+corpus = DocumentCorpus(["PubMed"])
+
+SCORE_FUNCTIONS = [
+    ("tfidf+conf", score_edge),
+    ("tfidf+conf+sentence", score_edge_sentence)
+]
 
 
 def get_document_graph(document_id):
@@ -21,37 +31,33 @@ def get_document_graph(document_id):
     query = query.filter(Predication.document_collection == "PubMed")
     query = query.filter(Predication.document_id == document_id)
     query = query.filter(Predication.relation.isnot(None))
-    facts = defaultdict(set)
+    facts = set()
     nodes = set()
+
+    docs = list(retriever.retrieve_narrative_documents_for_collections([document_id], ["PubMed"]))
+    doc = docs[0]
+
+    result = []
     for r in query:
         try:
             subject_name = resolver.get_name_for_var_ent_id(r.subject_id, r.subject_type, resolve_gene_by_id=False)
             object_name = resolver.get_name_for_var_ent_id(r.object_id, r.object_type, resolve_gene_by_id=False)
             subject_name = f'{subject_name} ({r.subject_type})'
             object_name = f'{object_name} ({r.object_type})'
-            if subject_name < object_name:
-                key = subject_name, r.relation, object_name
-                so_key = subject_name, object_name
-            else:
-                key = object_name, r.relation, subject_name
-                so_key = object_name, subject_name
+            key = subject_name, r.relation, object_name
             if key not in facts:
-                facts[so_key].add(r.relation)
+                scores = {}
+                for name, score_function in SCORE_FUNCTIONS:
+                    edge = (r.subject_id, r.relation, r.object_id)
+                    scores[name] = round(score_function(edge, doc, corpus), 2)
+
+                result.append(dict(s=subject_name, p=r.relation, o=object_name, scores=scores))
+                facts.add(key)
                 nodes.add(subject_name)
                 nodes.add(object_name)
         except Exception:
             pass
 
-    result = []
-    for (s, o), predicates in facts.items():
-        p_txt = []
-        for p in predicates:
-            # if there is a more specific edge then associated, ignore it
-            if len(predicates) > 1 and p == "associated":
-                continue
-            p_txt.append(p)
-        p_txt = '|'.join([pt for pt in p_txt])
-        result.append(dict(s=s, p=p_txt, o=o))
     print(f'Querying document graph for document id: {document_id} - {len(facts)} facts found')
     session.remove()
     return str(dict(nodes=list(nodes), facts=result))
@@ -61,7 +67,7 @@ def get_document_content(document_id):
     session = SessionExtended.get()
 
     doc = (retrieve_narrative_documents_from_database(session, document_ids={document_id},
-                                                          document_collection="PubMed")[0])
+                                                      document_collection="PubMed")[0])
     doc.title = doc.title.replace('\'', ' ')
     doc.abstract = doc.abstract.replace('\'', ' ')
     return str(doc.to_dict())
