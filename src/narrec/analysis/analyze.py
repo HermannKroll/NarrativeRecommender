4a3fe528ccae337605b5fd2e7ca07603bd77e204
HermannKroll <hermann.kroll@gmail.com>
2024-05-06 08:11:55 +0200

    add ratios and compute cores

--- src/narrec/analysis/analyze.py
+++ src/narrec/analysis/analyze.py
@@ -3,7 +3,10 @@ from tqdm import tqdm
 from narrant.entitylinking.enttypes import DRUG
 from narrec.backend.retriever import DocumentRetriever
 from narrec.benchmark.benchmark import Benchmark
-from narrec.run_config import BENCHMARKS
+from narrec.config import GLOBAL_DB_DOCUMENT_COLLECTION
+from narrec.document.core import NarrativeCoreExtractor
+from narrec.document.corpus import DocumentCorpus
+from narrec.run_config import BENCHMARKS, LOAD_FULL_IDF_CACHE
 
 
 def analyze_benchmark(retriever: DocumentRetriever, benchmark: Benchmark):
@@ -16,7 +19,11 @@ def analyze_benchmark(retriever: DocumentRetriever, benchmark: Benchmark):
                                                         document_collection=benchmark.document_collection)
     docid2docs = {d.id: d for d in input_docs}
 
-    print('Perform first stage retrieval')
+    corpus = DocumentCorpus(collections=[GLOBAL_DB_DOCUMENT_COLLECTION])
+    if LOAD_FULL_IDF_CACHE:
+        corpus.load_all_support_into_memory()
+
+    core_extractor = NarrativeCoreExtractor(corpus=corpus)
 
     graph_size2count = {}
     concept_count2count = {}
@@ -29,13 +36,22 @@ def analyze_benchmark(retriever: DocumentRetriever, benchmark: Benchmark):
         try:
             input_doc = docid2docs[int(doc_id)]
 
-            doc_graph_size = len(input_doc.graph)
+            core = core_extractor.extract_narrative_core_from_document(input_doc)
+            if core:
+                doc_graph_size = len(core.statements)
+            else:
+                doc_graph_size = 0
 
             if doc_graph_size not in graph_size2count:
                 graph_size2count[doc_graph_size] = 0
             graph_size2count[doc_graph_size] += 1
 
-            concept_count = len(input_doc.tags)
+            concept_core = core_extractor.extract_concept_core(input_doc)
+            if concept_core:
+                concept_count = len(concept_core.concepts)
+            else:
+                concept_count = 0
+
             if concept_count not in concept_count2count:
                 concept_count2count[concept_count] = 0
             concept_count2count[concept_count] += 1
@@ -51,7 +67,7 @@ def analyze_benchmark(retriever: DocumentRetriever, benchmark: Benchmark):
         except KeyError:
             print(f'Document {doc_id} not known in our collection - skipping')
 
-    ranges_to_repot = [(0, 5), (0, 10), (10, 100000)]
+    ranges_to_repot = [(0, 5), (0, 10), (5, 10000000), (10, 10000000)]
     print(f'Count of documents is: {no_docs}')
     for r_min, r_max in ranges_to_repot:
         sum_concepts = 0
@@ -63,8 +79,10 @@ def analyze_benchmark(retriever: DocumentRetriever, benchmark: Benchmark):
             if i in concept_count2count:
                 sum_concepts += concept_count2count[i]
 
-        print(f'Documents with concept count in range {r_min} - {r_max} - Sum: {sum_concepts}')
-        print(f'Documents with graph size    in range {r_min} - {r_max} - Sum: {sum_graph_size}')
+        ratio_con = 100 * round(sum_concepts / no_docs, 2)
+        ratio_graph = 100 * round(sum_graph_size / no_docs, 2)
+        print(f'Documents with concept count in range {r_min} - {r_max} - Sum: {sum_concepts} ({ratio_con}%)')
+        print(f'Documents with graph size    in range {r_min} - {r_max} - Sum: {sum_graph_size} ({ratio_graph}%)')
 
     print()
     print('Has a drug concept  : ', count_has_drug)
